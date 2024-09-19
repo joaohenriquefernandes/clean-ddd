ab5c6a2b2ca93a85f906f13b663e98326cb4f9a7
joaohenriquefernandes <joaohenriquefernandes.dev@gmail.com>
2024-09-18 22:35:15 -0300

    feat: create instructor entity

---
+++ src/domain/forum/enterprise/entities/instructor.ts
@@ -0,0 +1,14 @@
+import { Entity } from '@/core/entities/entity'
+import { UniqueEntityId } from '@/core/entities/unique-entity-id'
+
+interface IInstructorProps {
+  name: string
+}
+
+export class Instructor extends Entity<IInstructorProps> {
+  static create(props: IInstructorProps, id?: UniqueEntityId) {
+    const instructor = new Instructor(props, id)
+
+    return instructor
+  }
+}
