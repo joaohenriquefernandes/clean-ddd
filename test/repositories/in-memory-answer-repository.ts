facfcfa2481ec7dfc9323506f66b5d4f8e4f8963
joaohenriquefernandes <joaohenriquefernandes.dev@gmail.com>
2024-09-23 10:23:48 -0300

    feat: create in memory answer repository

---
+++ test/repositories/in-memory-answer-repository.ts
@@ -0,0 +1,10 @@
+import { IAnswerRepository } from '@/domain/forum/application/repositories/answer-repository'
+import { Answer } from '@/domain/forum/enterprise/entities/answer'
+
+export class InMemoryAnswerRepository implements IAnswerRepository {
+  public items: Answer[] = []
+
+  async create(answer: Answer): Promise<void> {
+    this.items.push(answer)
+  }
+}
